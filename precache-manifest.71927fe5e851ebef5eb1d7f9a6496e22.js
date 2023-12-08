413d2fdc5c3260c00d78c6275c2e1db3a4988390
Mr-Rohit-Mishra <rohitmishraholy@gmail.com>
2023-12-08 16:40:27 +0530

    Updates

---
+++ precache-manifest.71927fe5e851ebef5eb1d7f9a6496e22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84af1fa9e1ff42583f7c9d9768d19068",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "94efeea060669539e150",
+    "url": "/my-app/static/css/main.12466182.chunk.css"
+  },
+  {
+    "revision": "6b352a335b3509b0a418",
+    "url": "/my-app/static/js/2.c6a260b2.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/my-app/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "94efeea060669539e150",
+    "url": "/my-app/static/js/main.bfb8b2c4.chunk.js"
+  },
+  {
+    "revision": "09e0b948311a61c5c54a",
+    "url": "/my-app/static/js/runtime~main.a89e4939.js"
+  }
+]);
